5062156c1f19e9c1afd821f61443cb2de6f87069
Peerawat Srichaiyorak <peerawat.sri@ku.th>
2026-08-14 12:10:27 +0700

    function

--- labs/week06-lab/example/fortest.py
+++ labs/week06-lab/example/fortest.py
@@ -11,7 +11,7 @@ print("Calculating rectangle areas:")
 calculate_rectangle_area(5, 3)
 calculate_rectangle_area(10, 7)
 
-"""
+
 #geometry หาพื้นที่สามเหลี่ยม
 def calculate_triangle_area(height, base):
     area = 0.5 * height * base
@@ -19,9 +19,27 @@ def calculate_triangle_area(height, base):
 
 print("calculate")
 calculate_triangle_area(4, 10)
+"""
+"""
+เขียน Fucntion  ที่สามารถเเปลงเงินจาก
+THB <-> USD : USD = 32 THB 
 
 
+โดยใช้ชื่อ fuction convert_currency(100,USD)
 
+เเสดงผล
+100 THB = 3.3 USD
+"""
+def convert_currency(amount, currency):
+    if currency == "USD":
+        return amount / 32
+    elif currency == "THB":
+        return amount * 32
+    else:
+        return None
+ 
+print(f"100 THB = {convert_currency(100, 'USD')} USD")
+print(f"100 USD = {convert_currency(100, 'THB')} THB")
 
 
 
